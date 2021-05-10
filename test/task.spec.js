3b3fc48ebb706e54d3ff8baa838dae90397b7c71
eadogan <e.ahmetdogan@gmail.com>
2021-05-10 15:26:02 +0100

    Create test package

---
+++ test/task.spec.js
@@ -0,0 +1,34 @@
+let chai = require('chai')
+let chaiHttp = require('chai-http')
+let server = require('../index')
+
+// Assertion style chai.should
+chai.should()
+
+chai.use(chaiHttp)
+
+describe('Tasks API', () => {
+    /**
+     * Test the GET route
+     */
+
+
+    /**
+     * Test the GET By Id route
+     */
+
+
+    /**
+     * Test the POST route
+     */
+
+
+    /**
+     * Test the PATCH route
+     */
+
+
+    /**
+     * Test the DELETE route
+     */
+})
